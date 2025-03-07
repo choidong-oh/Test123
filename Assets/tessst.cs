9c9f266247a475d930b7f59799d51b1a6e6cec71
choidong-oh <qltjfeo@naver.com>
2025-03-07 23:06:36 +0900

    오목 기능 구현
    바둑판 구현끝

--- Assets/tessst.cs
+++ Assets/tessst.cs
@@ -1,40 +1,166 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class tessst : MonoBehaviour
 {
-    public GameObject prefabs;
-    public GameObject Tr;
-    int count = 0;
+    public GameObject Black;
+    public GameObject White;
+    public GameObject[] Tr;
+    int numA = 1;
+    int numB = 1;
 
 
-    public void OnButton1()
+    public void OnButton1(int number)
     {
-        var dd = Instantiate(prefabs, Tr.transform.position, Quaternion.identity);
-        count += Direction(Vector3.up);
-        //count += Direction(Vector3.right);  
-        //count += Direction(Vector3.left);  
-        //count += Direction(Vector3.down);  
-        
-
+        //다른플레이어
+        PlayerA(number);
+        //PlayerB();
     }
 
-    private int Direction(Vector3 direction)
+    void PlayerA(int number)
     {
-        int count = 1;
-        Vector3 startPosition = Tr.transform.position;
+        var dd = Instantiate(Black, Tr[number].transform.position, Quaternion.identity);
+        if (numA != 5)
+        {
+            numA += DirectionA(Vector3.up, number);
+            numA += DirectionA(Vector3.down, number);
+            if (numA == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numA = 1;
+            }
+
+        }
+        if (numA != 5)
+        {
+            numA += DirectionA(Vector3.right, number);
+            numA += DirectionA(Vector3.left, number);
+            if (numA == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numA = 1;
+            }
+
+        }
+
+        if (numA != 5)
+        {
+            numA += DirectionA(Vector3.right + Vector3.up,number);
+            numA += DirectionA(Vector3.left - Vector3.up, number);
+            if (numA == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numA = 1;
+            }
+
+        }
+        if (numA != 5)
+        {
+            numA += DirectionA(Vector3.left + Vector3.up, number);
+            numA += DirectionA(Vector3.right - Vector3.up, number);
+            if (numA == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numA = 1;
+            }
+
+        }
+        Debug.Log(numA);
+    }
+    void PlayerB(int number)
+    {
+        var dd = Instantiate(White, Tr[number].transform.position, Quaternion.identity);
+        if (numB != 5)
+        {
+            numB += DirectionB(Vector3.up, number);
+            numB += DirectionB(Vector3.down, number);
+            if (numB == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numB = 1;
+            }
+
+        }
+        if (numB!= 5)
+        {
+            numB += DirectionB(Vector3.right, number);
+            numB += DirectionB(Vector3.left, number);
+            if (numB == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numB = 1;
+            }
+
+        }
+
+        if (numB != 5)
+        {
+            numB += DirectionB(Vector3.right + Vector3.up, number);
+            numB += DirectionB(Vector3.left - Vector3.up, number);
+            if (numB == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numB = 1;
+            }
+
+        }
+        if (numB != 5)
+        {
+            numB += DirectionB(Vector3.left + Vector3.up, number);
+            numB += DirectionB(Vector3.right - Vector3.up, number);
+            if (numB == 5)
+            {
+                Debug.Log("게임끝");
+            }
+            else
+            {
+                numB = 1;
+            }
+
+        }
+        Debug.Log(numB);
+    }
+
+
+    private int DirectionA(Vector3 direction, int number)
+    {
+        int count = 0;
+        Vector3 startPosition = Tr[number].transform.position;
 
         for (int i = 0; i < 4; i++) 
         {
-            Ray ray = new Ray(new Vector3(startPosition.x, startPosition.y+i*0.5f, startPosition.z),direction); 
+            Vector3 rayOrigin = startPosition + (direction * i * 0.5f); // 이동 방향 적용
+            Ray ray = new Ray(rayOrigin, direction);
+            //Ray ray = new Ray(new Vector3(startPosition.x, startPosition.y+i*0.5f, startPosition.z),direction); 
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 0.5f))
             {
                 if (hit.collider.CompareTag("Black"))
                 {
-        Debug.Log("레이솜");
                     count++;
                 }
                 else
@@ -45,6 +171,37 @@ public class tessst : MonoBehaviour
             else
             {
                 break; 
+            }
+        }
+
+        return count;
+    }
+    private int DirectionB(Vector3 direction,int number)
+    {
+        int count = 0;
+        Vector3 startPosition = Tr[number].transform.position;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 rayOrigin = startPosition + (direction * i * 0.5f); // 이동 방향 적용
+            Ray ray = new Ray(rayOrigin, direction);
+            //Ray ray = new Ray(new Vector3(startPosition.x, startPosition.y+i*0.5f, startPosition.z),direction); 
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 0.5f))
+            {
+                if (hit.collider.CompareTag("White"))
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                break;
             }
         }
 
