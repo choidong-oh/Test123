93773fa33e627514ab725e148424e2677813a34f
choidong-oh <qltjfeo@naver.com>
2025-03-10 22:57:32 +0900

    ui디버그 추가했음

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
@@ -8,16 +9,37 @@ using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
-    private void Awake()
-    {
-        
-    }
+    public Text debugText;
+    public ScrollRect scrollRect;
+
     private void Start()
     {
         // 씬 동기화 설정 (마스터 클라이언트가 씬을 변경하면 자동으로 동기화됨)
         PhotonNetwork.AutomaticallySyncScene = true;
         Screen.SetResolution(1366, 768, false);
+
+        Application.logMessageReceived += HandleLog;
     }
+
+    //삭제도 생각해야댐, 메모리 누수
+    private void OnDestroy()
+    {
+        Application.logMessageReceived -= HandleLog; 
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        string logMessage = $"[{type}] {logString}\n"; // 로그 타입과 메시지 조합
+
+        if (debugText != null)
+        {
+            debugText.text += logMessage; // UI Text에 추가
+        }
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 0;
+
+    }
+
     public override void OnLeftRoom() //방 나가면 알아서 호출
     {
         SceneManager.LoadScene(0); //타이틀로 이동
